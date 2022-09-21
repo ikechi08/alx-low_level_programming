c4611312ffd5f7558ef080de7c108cc8cb720fdd
Henry <ikechi@yahoo.com>
2022-09-21 04:01:12 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,25 +1,25 @@
-nclude <stdlib.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 
 /**
- *  * main - Prints alphabet
- *   * @void: Empty parameter list for main.
- *    *
- *     * Description: Prints the alphabet in lower case
- *      * Can only use putchar
- *       *
- *        * Return: 0 for success
- *        */
+ * main - Prints alphabet
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints the alphabet in lower case
+ * Can only use putchar
+ *
+ * Return: 0 for success
+ */
 int main(void)
 {
-		char letter = 'a';
+	char letter = 'a';
 
-			while (letter <= 'z')
-					{
-								putchar(letter);
-										letter++;
-											}
-				putchar('\n');
-					return (0);
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
 }
